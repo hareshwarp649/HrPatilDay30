4a21e1e853bd8077c2c70645e042bf3ba87268b7
Harry <hareshwarp649>
2022-03-27 12:46:05 +0530

    Calculate Aggregate Total for all

---
+++ MultipleRides2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleRides2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Invoice Genrator Program");
+        }
+    }
+}
---
+++ MultipleRides2/Rides.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleRides2
+{
+    public class Rides
+    {
+        public double distance { get; set; }
+        public int time { get; set; }
+    }
+}
