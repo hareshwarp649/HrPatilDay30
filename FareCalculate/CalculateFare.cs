baf9447bd7ee77eb4d371d1b0b425449c7722351
Harry <hareshwarp649>
2022-03-27 12:07:33 +0530

    Calculate Total Fare Of Journey

---
+++ FareCalculate/CalculateFare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareCalculate
+{
+    public class CalculateFare
+    {
+        const int COST_PER_KM = 10;
+        const int COST_PER_MINUTE = 1;
+        const int MINIMUM_FAIR = 5;
+
+        public double CalculateFair(double distance, int time)
+        {
+            var fair = (distance * COST_PER_KM) + (time * COST_PER_MINUTE);
+            if (fair > MINIMUM_FAIR)
+            {
+                return fair;
+            }
+            return MINIMUM_FAIR;
+        }
+    }
+}
